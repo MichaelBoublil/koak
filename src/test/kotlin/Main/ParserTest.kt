f126e328b89e01b240f42ae82ca7282d31d5d3f2
Michael Boublil <michaelboublil@gmail.com>
2018-02-13 20:10:33 +0100

    ajout de tests unitaires

--- src/test/kotlin/Main/ParserTest.kt
+++ src/test/kotlin/Main/ParserTest.kt
@@ -11,30 +11,74 @@ import Parser.PegParser
 
 class ParserTest: Spek({
     given("A parser") {
-        val parser = PegParser("def fun(x : int) : int 2 + x;")
+        val parser = PegParser()
         on("Function with int parameter and int return value. Correct syntax.") {
+            parser.setString("def fun(x : int) : int 2 + x;")
             val tree = parser.parse()
-            val ref = AST(
-                    LocalDef(
-                            Prototype(
-                                    Identifier("fun"),
-                                    PrototypeArgs(
-                                            Params(
-                                                    Identifier("x"),
-                                                    VarType("int")
-                                            ),
-                                            FunType("int"))
-                            ),
-                            Expressions(
-                                    Expression(
-                                            Unary(PostFix(Primary(Literal(DecimalConst("2"))))),
-                                            BinOp("+", true),
-                                            Unary(PostFix(Primary(Identifier("x"))))
-                                    )
-                            )
-                    )
-            );
             it("should return the following value") {
+                val ref = AST(
+                        LocalDef(
+                                Prototype(
+                                        Identifier("fun"),
+                                        PrototypeArgs(
+                                                Params(
+                                                        Identifier("x"),
+                                                        VarType("int")
+                                                ),
+                                                FunType("int"))
+                                ),
+                                Expressions(
+                                        Expression(
+                                                Unary(PostFix(Primary(Literal(DecimalConst("2"))))),
+                                                BinOp("+", false),
+                                                Unary(PostFix(Primary(Identifier("x"))))
+                                        )
+                                )
+                        )
+                );
+                println("REFERENCE: " + ref.dump());
+                assertEquals(tree.dump(), ref.dump())
+            }
+        }
+        on("Simple Command Hello World. Correct Syntax") {
+            parser.setString("putchar(48);")
+            val tree = parser.parse()
+
+            it("should return the following value") {
+                val ref = AST(
+                        TopExpr(
+                                Expressions(
+                                        Expression(
+                                                Unary(PostFix(
+                                                        Primary(Identifier("putchar")),
+                                                        CallExpr(Expression(Unary(PostFix(Primary(Literal(DecimalConst("48")))))))
+                                                ))
+                                        )
+                                )
+                        )
+                );
+                println("REFERENCE: " + ref.dump());
+                assertEquals(tree.dump(), ref.dump())
+            }
+        }
+
+        // Ce test suggererait qu'on fait en effet l'inférence de type.
+        on("Simple Variable affectation WITH inference of type") {
+            parser.setString("x = 10;")
+            val tree = parser.parse()
+
+            it("should return the following value") {
+                val ref = AST(
+                        TopExpr(
+                                Expressions(
+                                        Expression(
+                                                Unary(PostFix(Primary(Identifier("x")))),
+                                                BinOp("=", true),
+                                                Unary(PostFix(Primary(Literal(DecimalConst("10")))))
+                                        )
+                                )
+                        )
+                );
                 println("REFERENCE: " + ref.dump());
                 assertEquals(tree.dump(), ref.dump())
             }
