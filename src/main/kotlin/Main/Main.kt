58b81368301e7a589f2d827ba3570500fb05ba46
Clara Girard <clara.girard@epitech.eu>
2018-02-15 18:56:26 +0100

    lala

--- src/main/kotlin/Main/Main.kt
+++ src/main/kotlin/Main/Main.kt
@@ -5,7 +5,7 @@ import Parser.*
 fun main(args: Array<String>) {
     var p = PegParser()
 
-    p.setString("putchar(0x34);")
+    p.setString("d+2;")
     val lol = p.parse()
     if (lol.nodes.isEmpty())
         println("ERROR")
