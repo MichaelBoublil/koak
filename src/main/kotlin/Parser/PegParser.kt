b54d53659368418dc76178263c2f44c3b7a81299
Clara Girard <clara.girard@epitech.eu>
2018-02-15 15:04:07 +0100

    try postfix + dec  oct hewa

--- src/main/kotlin/Parser/PegParser.kt
+++ src/main/kotlin/Parser/PegParser.kt
@@ -1,5 +1,7 @@
 package Parser
 
+import com.sun.corba.se.impl.resolver.INSURLOperationImpl
+
 class PegParser(private var _str : String? = null) {
 
     fun setString(str: String) {
@@ -15,19 +17,97 @@ class PegParser(private var _str : String? = null) {
     }
 
     private fun startParse() {
-        isIdentifier(_str)
+        println("La string au debut : " + _str)
+        val ret = isPostFix(_str)
+        println("str a la fin : " + ret.first)
+        if (ret.second != null) {
+            println("pas null")
+        }
+
+        //isIdentifier(_str)
     }
 
-    private fun isIdentifier(str : String?) : Pair<String?, AST?> {
+    private fun isPostFix(str : String?) : Pair<String?, INode?> {
+        val ret = isPrimary(str)
+        return when (ret.second) {
+            null -> Pair(str, null)
+            else -> {
+                val ret1 = isCallExpr(ret.first)
+                val node = ret.second!!
+                Pair(ret.first, PostFix(node))
+            }
+        }
+    }
+
+    private fun isCallExpr(str: String?) : Pair<String?, INode?> {
+        return Pair(str, null)
+    }
+
+    private fun isPrimary(str: String?) : Pair<String?, INode?> {
+        val ret = isIdentifier(str)
+        return when (ret.second) {
+            null -> Pair(str, null)
+            else -> {
+                val node = ret.second!!
+                Pair(ret.first, Primary(node))
+            }
+        }
+    }
+
+    private fun isHexadecimalDigit(str : String?, nb : String) : Pair<String?, INode?> {
+        return when (str!!.first() in '0'..'9' ||
+                     str.first() in 'a'..'f' ||
+                     str.first() in 'A'..'F') {
+            true -> isHexadecimalDigit(str.drop(1), nb + str.first())
+            false -> {
+                return when (nb.length) {
+                    0 -> Pair(str, null)
+                    else -> return (Pair(str, HexadecimalDigit(nb)))
+                }
+            }
+        }
+    }
+
+    private fun isOctalDigit(str : String?, nb : String) : Pair<String?, INode?> {
+        return when (str!!.first() in '0'..'7') {
+            true -> isOctalDigit(str.drop(1), nb + str.first())
+            false -> {
+                return when (nb.length) {
+                    0 -> Pair(str, null)
+                    else -> return (Pair(str, OctalDigit(nb)))
+                }
+            }
+        }
+    }
+
+    private fun isDecimalConst(str : String?, nb : String) : Pair<String?, INode?> {
+        return when (str!!.first().isDigit()) {
+            true -> isDecimalConst(str.drop(1), nb + str.first())
+            false -> {
+                return when (nb.length) {
+                    0 -> Pair(str, null)
+                    else -> return (Pair(str, DecimalConst(nb)))
+                }
+            }
+        }
+    }
+
+    private fun isIdentifier(str : String?) : Pair<String?, INode?> {
         if (str!!.first().isLetter()) {
-             return finishIdentifier(str.drop(1))
+            return finishIdentifier(str.drop(1), "" + str.first())
         }
         return Pair(str, null)
     }
 
-    private fun finishIdentifier(str : String?) : Pair<String?, AST?> {
-        println(str)
-
-        return Pair(str, null)
+    private fun finishIdentifier(str : String?, identifier : String) : Pair<String?, INode?> {
+        return when (str!!.first().isLetterOrDigit()) {
+            true -> finishIdentifier(str.drop(1), identifier + str.first())
+            false -> {
+                return when (identifier.length) {
+                    0 -> Pair(str, null)
+                    else -> return (Pair(str, Identifier(identifier)))
+                }
+            }
+        }
     }
 }
