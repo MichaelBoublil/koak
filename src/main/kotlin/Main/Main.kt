729d3acaf7372b54f50747ffed654d1b87dbf54c
Michael Boublil <michaelboublil@gmail.com>
2018-02-15 21:14:21 +0100

    front end en cours

--- src/main/kotlin/Main/Main.kt
+++ src/main/kotlin/Main/Main.kt
@@ -3,12 +3,18 @@ package Main
 import Parser.*
 
 fun main(args: Array<String>) {
-    var p = PegParser()
-
-    p.setString("putchar(0x34);")
-    val lol = p.parse()
-    if (lol.nodes.isEmpty())
-        println("ERROR")
-    else
-        println(lol.dump())
+//    var p = PegParser()
+//
+//    p.setString("putchar(0x34);")
+//    val lol = p.parse()
+//    if (lol.nodes.isEmpty())
+//        println("ERROR")
+//    else
+//        println(lol.dump())
+    try {
+        FrontEnd(args)
+    }
+    catch (e : Exception) {
+        e.printStackTrace()
+    }
 }
--- src/main/kotlin/Messages/Errors.kt
+++ src/main/kotlin/Messages/Errors.kt
@@ -1,4 +1,5 @@
 package Messages
 
 val nullString : String = "Error: Cannot parse null string."
+val wrongFileExtension : String = "Error: cannot parse a file that does not end with .koak"
 
