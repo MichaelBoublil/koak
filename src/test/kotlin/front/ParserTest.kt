e9fe9695b1b96ab144cc962b7b91080482e24ec0
Michael Boublil <michaelboublil@gmail.com>
2018-02-26 16:32:01 +0100

    test unitaire

--- src/test/kotlin/front/ParserTest.kt
+++ src/test/kotlin/front/ParserTest.kt
@@ -12,34 +12,38 @@ import Parser.PegParser
 class ParserTest: Spek({
     given("A parser") {
         val parser = PegParser()
-        /*on("Function with int parameter and int return value. Correct syntax.") {
+        on("Function with int parameter and int return value. Correct syntax.") {
             parser.setString("def fun(x : int) : int 2 + x;")
             val tree = parser.parse()
             it("should return the following value") {
                 val ref = AST(
-                        LocalDef(
-                                Prototype(
-                                        Identifier("fun"),
-                                        PrototypeArgs(
-                                                Params(
-                                                        Identifier("x"),
-                                                        VarType("int")
+                        KDefs(
+                                LocalDef(
+                                        Defs(
+                                                Prototype(
+                                                        Identifier("fun"),
+                                                        PrototypeArgs(
+                                                                Args(
+                                                                        Identifier("x"),
+                                                                        VarType("int")
+                                                                ),
+                                                                FunType("int"))
                                                 ),
-                                                FunType("int"))
-                                ),
-                                Expressions(
-                                        Expression(
-                                                Unary(PostFix(Primary(Literal(DecimalConst("2"))))),
-                                                BinOp("+", false),
-                                                Unary(PostFix(Primary(Identifier("x"))))
+                                                Expressions(
+                                                        Expression(
+                                                                BinOp("+", false,
+                                                                        Unary(PostFix(Primary(Literal(DecimalConst("2"))))),
+                                                                        Unary(PostFix(Primary(Identifier("x"))))
+                                                                )
+                                                        )
+                                                )
                                         )
                                 )
                         )
-                );
-                println("REFERENCE: " + ref.dump());
+                )
                 assertEquals(tree.dump(), ref.dump())
             }
-        }*/
+        }
         on("Simple Command Hello World. Correct Syntax") {
             parser.setString("putchar(48);")
             val tree = parser.parse()
@@ -59,8 +63,6 @@ class ParserTest: Spek({
                                 )
                         )
                 );
-                println("REFERENCE: " + ref.dump());
-                println("ACTUAL: " + tree.dump());
                 assertEquals(ref.dump(), tree.dump())
             }
         }
